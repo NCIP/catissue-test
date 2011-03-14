b34a6ce5cfad98307fd1a35cb25307ff434783d1
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2011-03-14 11:36:28 +0000

    SVN-Revision: 25443

--- script/Clinportal_User_Update_Oracle.sql
+++
@@ -1,9 +0,0 @@
-#script to update date in CSM_User table
-
-REMARK "Upading date in CSM_USER
-update csm_user set update_date=CURRENT_DATE where login_name='clin_portal@persistent.co.in';
-
-REMARK "Upading date in CATISSUE_PASSWORD
-update catissue_password set update_date=CURRENT_DATE where user_id=(select user_id from csm_user where login_name='clin_portal@persistent.co.in');
-
-quit;
