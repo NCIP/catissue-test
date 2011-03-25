28170a64379cd95c35975d5fcf001e62653f3d7e
Nitesh Marwaha <nitesh.marwaha@krishagni.com>
2011-03-25 13:32:37 +0000

    SVN-Revision: 25602

--- shutdown.pl
+++ shutdown.pl
@@ -1,15 +1,29 @@
 # Search for the jboss process for the caTissue Nightly Build for BO Automation test script execution
-$grep = `ps -ef | grep "jboss-4.2.2.G*" | grep -v grep | awk '{print $2}'`;
+$grep = `ps -ef | grep "jboss" | grep -v grep | awk '{print $2}'`;
 
 print "$grep\n";
 
+@tasklist = split('\n',$grep);
+#print "$tasklist[0]\n";
+#print "$tasklist[1]\n";
+print "_________________________________________________________";
+foreach $value (@tasklist)
+{
 # Split the string for process
-@taskid = split(' ',$grep);
+#print "$value\n";
+@taskid = split(' ',$value);
 
 # Print the Process Id
-print "$taskid[1]";
+print "$taskid[1]\n";
+$kill = `kill -9 $taskid[1]`;
+print "#############";
+}
+# Split the string for process
+#@taskid = split(' ',$grep);
+
+# Print the Process Id
+#print "$taskid[1]";
 
 # Kill the Process
 
-$kill = `kill -9 $taskid[1]`;
-
+#$kill = `kill -9 $taskid[1]`;
